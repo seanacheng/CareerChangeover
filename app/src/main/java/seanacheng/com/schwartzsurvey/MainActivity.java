ae0c1bd948291ef3551a654764731a88f325d2e3
Sean <sean.cheng@intelluslearning.com>
2018-07-15 16:16:19 -0700

    first commit

---
+++ app/src/main/java/seanacheng/com/schwartzsurvey/MainActivity.java
@@ -0,0 +1,41 @@
+package seanacheng.com.schwartzsurvey;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button identify_values, view_results;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        identify_values = findViewById(R.id.identify_values);
+        view_results = findViewById(R.id.view_results);
+        identify_values.setOnClickListener(this);
+        view_results.setOnClickListener(this);
+        Log.d("tag", "onCreate");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.identify_values:
+                Intent toSurveyMenu = new Intent(MainActivity.this,SurveyMenu.class);
+                startActivity(toSurveyMenu);
+                Log.d("tag", "onClick: identify values");
+                break;
+            case R.id.view_results:
+                Log.d("tag","onClick: view results");
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/seanacheng/com/schwartzsurvey/SurveyMenu.java
@@ -0,0 +1,13 @@
+package seanacheng.com.schwartzsurvey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveyMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_menu);
+    }
+}
