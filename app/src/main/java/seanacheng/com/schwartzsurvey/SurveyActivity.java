f2b1777b24059ef8b9cf667c490ee9689008e116
Sean <sean.cheng@intelluslearning.com>
2018-07-17 20:41:47 -0700

    refactored strings

--- app/src/main/java/seanacheng/com/schwartzsurvey/SurveyActivity.java
+++ app/src/main/java/seanacheng/com/schwartzsurvey/SurveyActivity.java
@@ -2,12 +2,23 @@ package seanacheng.com.schwartzsurvey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SurveyActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
     }
+
 }
