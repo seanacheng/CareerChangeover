09b496caf935b7ae834bff9fdb87cf9ee23b3174
Sean <sean.cheng@intelluslearning.com>
2018-08-16 00:01:11 -0500

    updated table to show outline

--- app/src/main/java/seanacheng/com/schwartzsurvey/DescriptionsActivity.java
+++ app/src/main/java/seanacheng/com/schwartzsurvey/DescriptionsActivity.java
@@ -1,11 +1,25 @@
 package seanacheng.com.schwartzsurvey;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.text.Layout;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DescriptionsActivity extends AppCompatActivity {
 
@@ -13,21 +27,50 @@ public class DescriptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_descriptions);
-        TableLayout descriptionsTable = findViewById(R.id.descriptionsTable);
+        TableLayout descriptionsTable = findViewById(R.id.descriptionsGrid);
         String[] descriptions = getResources().getStringArray(R.array.value_groupings);
 
-        int i = 1;
-        for (String desc:descriptions) {
-            String numbered = i+") "+desc;
-            i++;
+        for (int i=0;i<descriptions.length;i=i+2) {
+            LinearLayout horizontalRow = new LinearLayout(this);
+            horizontalRow.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView name = new TextView(this);
+            name.setText(descriptions[i]);
+            name.setTextColor(getResources().getColor(R.color.navy));
+            name.setWidth(getScreenResolution(this).get("width")*3/10);
+            name.setBackground(getResources().getDrawable(R.drawable.cell_border));
+            horizontalRow.addView(name);
 
             TextView description = new TextView(this);
-            description.setText(numbered);
-            description.setTextColor(getResources().getColor(R.color.darkgray));
-            description.setPadding(0,0,0,10);
+            description.setText(descriptions[i+1]);
+            description.setTextColor(getResources().getColor(R.color.navy));
+            description.setWidth(getScreenResolution(this).get("width")*13/20);
+            description.setBackground(getResources().getDrawable(R.drawable.cell_border));
+            horizontalRow.addView(description);
+            descriptionsTable.addView(horizontalRow);
 
-            descriptionsTable.addView(description);
+            description.measure(0,0);
+            int rowHeight = description.getMeasuredHeight();
+            name.setHeight(rowHeight);
+
         }
 
+    }
+
+    private static Map<String, Integer> getScreenResolution(Context context) {
+        // Returns map of width and height of screen
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        int density = metrics.densityDpi;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("width",width);
+        map.put("height",height);
+        map.put("density",density);
+        return map;
     }
 }
